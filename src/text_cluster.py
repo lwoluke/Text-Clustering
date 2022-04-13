de9a88c8acf7806bb978e14c5b198169f2ad4acb
lwoluke <lw04ostr@siena.edu>
2022-04-13 11:16:32 -0400

    added training data

--- src/text_cluster.py
+++ src/text_cluster.py
@@ -1,1 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Cluster the descriptions of Siena's courses
 
+Reminder: Your models should be built ONLY from the descriptions. 
+The other three files are there ONLY so that you can compute an ARI score.
+
+@author: Luke Ostrander
+"""
+
+import numpy as np
+
+mydir = "Z:/CSIS 320/Projects/Text-Clustering/data/"
+descriptions = np.loadtxt(mydir + "descriptions.txt", dtype="str", delimiter="\t", skiprows=1)
