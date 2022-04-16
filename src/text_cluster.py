43ed29dcfed8a86005a0e59a13ceaf603de39a1e
Luke Ostrander <lw04ostr@siena.edu>
2022-04-15 22:33:02 -0400

    minor changes

--- src/text_cluster.py
+++ src/text_cluster.py
@@ -22,7 +22,8 @@ from sklearn.neural_network import MLPClassifier
 # load training data
 mydir = "C:/Users/lwolu/OneDrive/Documents/College/Siena/Spring 2022 Semester/CSIS 320 - Machine Learning/Projects/Text-Clustering/data/"
 descriptions = np.loadtxt(mydir + "descriptions.txt", dtype="str", delimiter="\t", skiprows=1)
-descriptions_vec = CountVectorizer().fit(descriptions)
+descriptions_data = descriptions[:,1]
+descriptions_vec = CountVectorizer(max_df=.9).fit_transform(descriptions_data)
 
 '''
 Clusters
@@ -36,17 +37,17 @@ Last is to be determined from testing
 
 Create for K-Means, Agglomerative, and LDA to compare which is best for various tests
 '''
-kmeans_schools = KMeans(n_clusters=3, random_state=0).fit(descriptions_vec)
-kmeans_departments = KMeans(n_clusters=37, random_state=0).fit(descriptions_vec)
-kmeans_courses = KMeans(n_clusters=57, random_state=0).fit(descriptions_vec)
+kmeans_schools = KMeans(n_clusters=3, random_state=42).fit(descriptions_vec)
+kmeans_departments = KMeans(n_clusters=37, random_state=42).fit(descriptions_vec)
+kmeans_courses = KMeans(n_clusters=57, random_state=42).fit(descriptions_vec)
 
-agglo_schools = AgglomerativeClustering(n_clusters=3, random_state=0).fit(descriptions_vec)
-agglo_departments = AgglomerativeClustering(n_clusters=37, random_state=0).fit(descriptions_vec)
-agglo_courses = AgglomerativeClustering(n_clusters=57, random_state=0).fit(descriptions_vec)
+agglo_schools = AgglomerativeClustering(n_clusters=3).fit(descriptions_vec)
+agglo_departments = AgglomerativeClustering(n_clusters=37).fit(descriptions_vec)
+agglo_courses = AgglomerativeClustering(n_clusters=57).fit(descriptions_vec)
 
-lda_schools = LinearDiscriminantAnalysis(n_components=3, random_state=0).fit(descriptions_vec)
-lda_departments = LinearDiscriminantAnalysis(n_components=37, random_state=0).fit(descriptions_vec)
-lda_courses = LinearDiscriminantAnalysis(n_components=57, random_state=0).fit(descriptions_vec)
+lda_schools = LinearDiscriminantAnalysis(n_components=3, random_state=42).fit(descriptions_vec)
+lda_departments = LinearDiscriminantAnalysis(n_components=37, random_state=42).fit(descriptions_vec)
+lda_courses = LinearDiscriminantAnalysis(n_components=57, random_state=42).fit(descriptions_vec)
 
 '''
 Use best parameter settings from above and try several different numbers of groups
@@ -54,6 +55,6 @@ Use best parameter settings from above and try several different numbers of grou
 Suggestion: start with 2,4,6,10,15,20,30
 The optimal grouping will be the one with the highest silhouette score
 '''
-kmeans_schools = KMeans(n_clusters=2, random_state=0).fit(descriptions_vec)
-agglo_schools = AgglomerativeClustering(n_clusters=2, random_state=0).fit(descriptions_vec)
-lda_schools = LinearDiscriminantAnalysis(n_components=2, random_state=0).fit(descriptions_vec)
+kmeans_schools = KMeans(n_clusters=2, random_state=42).fit(descriptions_vec)
+agglo_schools = AgglomerativeClustering(n_clusters=2).fit(descriptions_vec)
+lda_schools = LinearDiscriminantAnalysis(n_components=2, random_state=42).fit(descriptions_vec)
