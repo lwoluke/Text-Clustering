1753e2a93eaf3e25692ceaa85479a208d737c934
Luke Ostrander <lw04ostr@siena.edu>
2022-04-16 14:15:28 -0400

    created necessary components for silouette

--- src/text_cluster.py
+++ src/text_cluster.py
@@ -14,7 +14,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.cluster import KMeans, DBSCAN
 from sklearn.cluster import AgglomerativeClustering
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.decomposition import LatentDirichletAllocation
+import scipy.sparse as sp
 from sklearn.feature_extraction.text import CountVectorizer, ENGLISH_STOP_WORDS, TfidfVectorizer
 from sklearn.model_selection import cross_val_score, GridSearchCV
 from sklearn.linear_model import LogisticRegression
@@ -26,10 +27,14 @@ from sklearn.decomposition import LatentDirichletAllocation
 # Load Training Data
 mydir = "C:/Users/lwolu/OneDrive/Documents/College/Siena/Spring 2022 Semester/CSIS 320 - Machine Learning/Projects/Text-Clustering/data/"
 file = np.loadtxt(mydir + "descriptions.txt", dtype="str", delimiter="\t", skiprows=1)
+key = file[:,0]
 descriptions = file[:,1]
 
-# Experiment with different max_df values in CountVectorizer
-desc_vec = CountVectorizer(max_df=0.2).fit_transform(descriptions)
+# Experiment with different max_df values in CountVectorizer (Use 0.2 as baseline)
+# Euclidean distance usually doesn't work well with count vectors. You might try it with tf-idf, though.
+desc_countvec = CountVectorizer(max_df=0.2).fit_transform(descriptions)
+desc_tfidfvec = TfidfVectorizer(max_df=0.2).fit_transform(descriptions)
+
 
 '''
 Clusters
@@ -43,30 +48,50 @@ Last is to be determined from testing
 
 Create for K-Means, Agglomerative, and LDA to compare which is best for various tests
 '''
-kmeans_schools = KMeans(n_clusters=3, random_state=42).fit(desc_vec)
-kmeans_departments = KMeans(n_clusters=37, random_state=42).fit(desc_vec)
-kmeans_courses = KMeans(n_clusters=57, random_state=42).fit(desc_vec)
+# kmeans_schools = KMeans(n_clusters=3, random_state=42).fit(desc_countvec)
+# kmeans_departments = KMeans(n_clusters=33, random_state=42).fit(desc_countvec)
+# kmeans_courses = KMeans(n_clusters=57, random_state=42).fit(desc_countvec)
 
-km_school_labels = kmeans_schools.labels_
-km_departments_labels = kmeans_departments.labels_
-km_courses_labels = kmeans_courses.labels_
+# km_school_labels = kmeans_schools.labels_
+# km_departments_labels = kmeans_departments.labels_
+# km_courses_labels = kmeans_courses.labels_
 
-# agglo_schools = AgglomerativeClustering(n_clusters=3).fit(descriptions_vec)
-# agglo_departments = AgglomerativeClustering(n_clusters=37).fit(descriptions_vec)
-# agglo_courses = AgglomerativeClustering(n_clusters=57).fit(descriptions_vec)
+# agglo_schools = AgglomerativeClustering(n_clusters=3).fit(desc_countvec)
+# agglo_departments = AgglomerativeClustering(n_clusters=33).fit(desc_countvec)
+# agglo_courses = AgglomerativeClustering(n_clusters=57).fit(desc_countvec)
 
-# lda_schools = LinearDiscriminantAnalysis(n_components=3, random_state=42).fit(descriptions_vec)
-# lda_departments = LinearDiscriminantAnalysis(n_components=37, random_state=42).fit(descriptions_vec)
-# lda_courses = LinearDiscriminantAnalysis(n_components=57, random_state=42).fit(descriptions_vec)
+# lda_schools = LatentDirichletAllocation(n_components=3, random_state=42).fit(desc_countvec)
+# lda_departments = LatentDirichletAllocation(n_components=33, random_state=42).fit(desc_countvec)
+# lda_courses = LatentDirichletAllocation(n_components=57, random_state=42).fit(desc_countvec)
 
 '''
 Use best parameter settings from above and try several different numbers of groups
 
 Suggestion: start with 2,4,6,10,15,20,30
 The optimal grouping will be the one with the highest silhouette score
-'''
-kmeans_best = KMeans(n_clusters=2, random_state=42).fit(desc_vec)
-km_best_labels = kmeans_best.labels_
 
-# agglo_best = AgglomerativeClustering(n_clusters=2).fit(descriptions_vec)
-# lda_best = LinearDiscriminantAnalysis(n_components=2, random_state=42).fit(descriptions_vec)
+When calculating silhouette coefficient:
+ The value is between [-1, 1]. A score of 1 denotes the best meaning that the 
+ data point i is very compact within the cluster to which it belongs and far away
+ from the other clusters. A value near 0 represents overlapping clusters with 
+ samples very close to the decision boundary of the neighboring clusters. 
+ A negative score indicates that the samples might have got assigned to the 
+ wrong clusters.
+'''
+
+### BEST FOR CountVectorizer ###
+kmeans_best = KMeans(n_clusters=2, random_state=42).fit(desc_tfidfvec)
+km_best_labels = kmeans_best.labels_
+kmeans_silhouette = silhouette_score(desc_countvec, kmeans_best.labels_, metric='manhattan')
+
+# agglo_best = AgglomerativeClustering(n_clusters=2).fit(desc_countvec.toarray())
+# agglo_best_labels = agglo_best.labels_
+# agglo_silhouette = silhouette_score(desc_countvec, agglo_best_labels, metric='manhattan')
+
+# lda_best = LatentDirichletAllocation(n_components=2, random_state=42).fit(desc_countvec)
+# desc_mat = sp.csr_matrix.toarray(desc_countvec)
+# post_topics = np.matmul(desc_mat, np.transpose(lda_best.components_))
+# lda_best_answers = np.argmax(post_topics, axis=1)
+# lda_silhouette = silhouette_score(desc_countvec, lda_best_answers, metric='manhattan')
+
+### BEST FOR TfidfVectorizer ###
